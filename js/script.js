c842e7cea4f8533eefc913b574f0ae96025fb625
vinnysts <vinnysts.dg@outlook.com.br>
2023-12-01 16:04:44 -0300

    Upload website DSL

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*function clickMenu(){
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+        }else{
+            itens.style.display = 'block'
+
+        }
+}*/
+
+function toggleImage(imageId) {
+    let image = document.getElementById(imageId);
+    let isGrayscale = JSON.parse(localStorage.getItem(imageId)) || false;
+
+    if (!isGrayscale) {
+        image.style.filter = "grayscale(100%)";
+        localStorage.setItem(imageId, true);
+        
+    } else {
+        image.style.filter = "none";
+        localStorage.setItem(imageId, false);
+    }
+}
+
+window.onload = function () {
+    let images = document.querySelectorAll('img');
+    images.forEach(function(image) {
+        let isGrayscale = JSON.parse(localStorage.getItem(image.id)) || false;
+        if (isGrayscale) {
+            image.style.filter = "grayscale(100%)";
+        }
+    });
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+### 🎮 DSL - DATASAVELIST 🏆 ###
+
+DESCRIÇÃO:
+
+Este projeto de website consiste em uma lista de controle pessoal, contendo títulos de games concluído e/ou em andamento, iniciados a partir de ano de 2005 até a data atual.
+
+STATUS:
+
+Esse website é puramente demonstrativo, servindo apenas como item de portfólio e está e atualmente se encontra em construção.
+
+
