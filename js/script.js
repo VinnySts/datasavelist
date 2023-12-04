064b6867a06f52eab8481877b98186cbc8a2a420
vinnysts <vinnysts.dg@outlook.com.br>
2023-12-04 18:13:08 -0300

    new update index page

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-/*function clickMenu(){
+function changeSize(){
+    if(window.innerWidth >= 768){
+        iten.style.display = 'block'
+    } else {
+        itens.style.display = 'none'
+    }
+}
+
+function clickMenu(){
     if (itens.style.display == 'block'){
         itens.style.display = 'none'
-        }else{
+    }else{
             itens.style.display = 'block'
-
-        }
-}*/
+    }
+}
 
 function toggleImage(imageId) {
     let image = document.getElementById(imageId);
