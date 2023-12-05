154f399e86f0829ea5ec1536546dc95c33bb9c34
vinnysts <vinnysts.dg@outlook.com.br>
2023-12-05 16:43:41 -0300

    Update menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function changeSize(){
     if(window.innerWidth >= 768){
-        iten.style.display = 'block'
+        itens.style.display = 'block'
     } else {
         itens.style.display = 'none'
     }
@@ -10,7 +10,7 @@ function clickMenu(){
     if (itens.style.display == 'block'){
         itens.style.display = 'none'
     }else{
-            itens.style.display = 'block'
+        itens.style.display = 'block'
     }
 }
 
